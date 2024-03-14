c3679b282450028be33bd6b4cdeba100d12d2bd5
JuanManuelCuartas <129427901+JuanCuartasE@users.noreply.github.com>
2024-03-14 18:36:37 +0100

    merge de signup y home sin navegar (static)

--- authentapp/urls.py
+++ authentapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.signup),
+    path('', views.home),
+    
 ]
--- authentapp/views.py
+++ authentapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def signup(request):
-    return render(request, 'signup.html')
+def home(request):
+    return render(request, 'home.html')
