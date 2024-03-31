ffb0002186c6383de4cf5aa9ac59dc9f296a94b0
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2024-03-31 00:42:22 -0500

    Se implementa la funcionalidad de registro de usuarios.

--- authentapp/views.py
+++ authentapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Usuario, Titulo, UsuarioTitulo
 
 def login(request):
     return render(request, 'login.html')
@@ -8,4 +9,30 @@ def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'GET':
+        return render(request, 'signup.html', {'titulos_disponibles': Titulo.objects.all()})
+    else:
+        if request.POST['password'] == request.POST['repassword']:
+            try:
+                user = Usuario.objects.create_user(
+                    username=request.POST['your_email'],
+                    password=request.POST['password'],
+                    first_name=request.POST['first_name'],
+                    last_name=request.POST['last_name'],
+                    email=request.POST['your_email'],
+                    documento=request.POST['doc_id'],
+                    codigo_pais=request.POST['code'],
+                    telefono=request.POST['phone']
+                    )
+                titulos = request.POST.getlist('titles[]')
+                for titulo_nombre in titulos:
+                    titulo = Titulo.objects.get(nombre=titulo_nombre)
+                    usuario_titulo = UsuarioTitulo.objects.create(
+                        id_usuario=user, id_titulo=titulo
+                        )
+                print(request.POST)
+                return redirect('../login')
+            except:
+                return render(request, 'signup.html', {'titulos_disponibles': Titulo.objects.all(), 'error': 'Ya existe un usuario registrado con ese correo.', 'data': request.POST})
+        return render(request, 'signup.html', {'titulos_disponibles': Titulo.objects.all(), 'error': 'Las contraseñas no coinciden.', 'data': request.POST})
+    
