67ffa9002d18a8cffb3cd328eda1ed35e6380283
JuanManuelCuartas <129427901+JuanCuartasE@users.noreply.github.com>
2024-04-16 10:36:39 -0500

    Crear ventana emergente de perfil

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('pr-info/', views.proyectos_info, name='proyecto-info'),
+    path('perfil-u/', views.perfil_usuario, name='perfil-usuario'),
 ]
