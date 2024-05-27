cd2a194632c094160ce0ad38eae6d5d1fd39c638
JuanManuelCuartas <129427901+JuanCuartasE@users.noreply.github.com>
2024-05-27 10:30:18 -0500

    Refactor URLs and update project user selection in proyectos.html

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
     path('proyectos/', views.proyectos_info, name='proyectos'),
     path('nuevo-proyecto/', views.nuevo_proyecto, name='nuevo-proyecto'),
     path('actualizar_perfil/', views.actualizar_perfil, name='actualizar_perfil'),
