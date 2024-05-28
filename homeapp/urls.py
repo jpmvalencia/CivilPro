55a32f5a8e322ec46415dd6e28db136385bd86e6
JuanManuelCuartas <129427901+JuanCuartasE@users.noreply.github.com>
2024-05-28 14:05:57 -0500

    Modificacion, bout a error line

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home),
     path('proyectos/', views.proyectos_info, name='proyectos'),
     path('nuevo-proyecto/', views.nuevo_proyecto, name='nuevo-proyecto'),
     path('actualizar_perfil/', views.actualizar_perfil, name='actualizar_perfil'),
