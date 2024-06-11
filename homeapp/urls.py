c6b64c2fa1f9c99dd163a49eaf043040e15aa22b
Melany Suarez <melanyjsuarez@gmail.com>
2024-06-10 20:53:16 -0500

    Se agrega la funcionalidad para eliminar tareas.

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('buscar-usuario/', views.buscar_usuario, name='buscar-usuario'),
     path('agregar-usuario/', views.agregar_usuario, name='agregar-usuario'),
     path('nueva-tarea/<int:id_proyecto>', views.nueva_tarea, name='nueva-tarea'),
-    path('eliminar-proyecto/<int:id_proyecto>/', views.eliminar_proyecto, name='eliminar_proyecto'),
+    path('eliminar-tarea/<int:id_tarea>', views.eliminar_tarea, name='eliminar_tarea'),
+    path('eliminar-proyecto/<int:id_proyecto>/', views.eliminar_proyecto, name='eliminar_proyecto')
 ]
 
 
