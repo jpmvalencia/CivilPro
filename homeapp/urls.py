7f0a9535b59bc7a2b58b84eb95f280234f7eb193
Melany Suarez <melanyjsuarez@gmail.com>
2024-06-10 20:13:13 -0500

    Validaciones en el front pe¿ara no ingresar numeros negativos en el presupuesto

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -11,8 +11,7 @@ urlpatterns = [
     path('buscar-usuario/', views.buscar_usuario, name='buscar-usuario'),
     path('agregar-usuario/', views.agregar_usuario, name='agregar-usuario'),
     path('nueva-tarea/<int:id_proyecto>', views.nueva_tarea, name='nueva-tarea'),
-    path('eliminar-proyecto/<int:id_proyecto>/', views.eliminar_proyecto, name='eliminar_proyecto')
-
+    path('eliminar-proyecto/<int:id_proyecto>/', views.eliminar_proyecto, name='eliminar_proyecto'),
 ]
 
 
